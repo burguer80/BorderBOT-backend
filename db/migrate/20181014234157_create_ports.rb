b6e2f937ad5d972803150e15993c6f89ae0d33d8
Sergio Burgueño <burguer@gmail.com>
2018-11-04 19:29:07 -0800

    setup dokku

--- db/migrate/20181014234157_create_ports.rb
+++ db/migrate/20181014234157_create_ports.rb
@@ -1,8 +1,17 @@
+# frozen_string_literal: true
+
 class CreatePorts < ActiveRecord::Migration[5.2]
   def change
     create_table :ports do |t|
+      t.timestamp :taken_at
+      t.string :status
+      t.jsonb :data
+      t.string :number
 
       t.timestamps
     end
+    add_index :ports, :data, using: :gin
+    add_index :ports, %i[taken_at number]
+    add_index :ports, :number
   end
 end
