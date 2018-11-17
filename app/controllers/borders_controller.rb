a1df21f9ac0f7efaf39334aa3ab8744ffb3d8e49
Sergio Burgueño <burguer@gmail.com>
2018-11-16 18:47:23 -0800

    cache total ports value

--- app/controllers/borders_controller.rb
+++ app/controllers/borders_controller.rb
@@ -4,11 +4,14 @@ class BordersController < ApplicationController
   def pull_data
     Port.pull_data
     Port.updatePortDetails
+    Rails.cache.write('total_ports', Port.count)
+
     render status: 200
   end
 
   def index
-    render json: { ports: Port.count}
+    ports_total = Rails.cache.fetch('total_ports') { Port.count }
+    render json: { ports: ports_total }
   end
 
 end
