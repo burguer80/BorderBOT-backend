85584f6f3486c7d63f93ae7b1b223ade0ddaa115
Sergio Burgueño <burguer@gmail.com>
2019-06-27 19:53:51 -0700

    remove unused endpoints

--- app/controllers/borders_controller.rb
+++ app/controllers/borders_controller.rb
@@ -1,22 +1,8 @@
 # frozen_string_literal: true
 
 class BordersController < ApplicationController
-  def pull_data
-    sd = SyncDataService.new
-    sd.pull_data
-
-    render status: 200
-  end
-
   def index
     ports_total = SyncDataService.new.ports_total
     render json: ports_total
-  end
-
-  def push_data
-    sd = SyncDataService.new
-    sd.push_to_firebase
-
-    render status: 200
   end
 end
