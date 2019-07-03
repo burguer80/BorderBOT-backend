cc459eb9f370ff8afa062e6f44447c596a1629f4
Sergio Burgueño <burguer@gmail.com>
2019-07-03 16:29:49 -0700

    add sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  mount Sidekiq::Web, at: "/sidekiq"
+
   get 'port/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'ports', to: 'port#index'
