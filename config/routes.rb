7ba14cd55078811dc4dbf406f602f734dfe9435f
Sergio Burgueño <burguer@gmail.com>
2019-07-04 18:40:28 -0700

    remove unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
 
   mount Sidekiq::Web, at: "/sidekiq"
-
-  get 'port/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'ports', to: 'port#index'
   root to: 'borders#index'
 
   # Dokku
