bbff524e0567bf9baaec03a60a14bdcb63b852e2
Sergio Burgueño <burguer@gmail.com>
2019-12-15 20:23:44 -0800

    setup github actions

--- spec/requests/health_spec.rb
+++ spec/requests/health_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe 'Health endpoint', type: :request do
+  RSpec.describe 'Health endpoint', type: :request do
 
-  describe 'GET root index' do
-    before { get '/' }
+    describe 'GET root index' do
+      before { get '/' }
 
-    it 'should return OK' do
-      payload = JSON.parse(response.body)
-      expect(payload).not_to be_empty
-    end
+      it 'should return OK' do
+        payload = JSON.parse(response.body)
+        expect(payload).not_to be_empty
+      end
 
-    it 'should return status code 200' do
-      expect(response).to have_http_status(200)
+      it 'should return status code 200' do
+        expect(response).to have_http_status(200)
+      end
     end
   end
-end
--- test/factories/ports.rb
+++ test/factories/ports.rb
@@ -1,8 +1,14 @@
 FactoryBot.define do
   factory :port do
     taken_at { Faker::Date.between(from: 2.days.ago, to: Date.today) }
-    status { "Open" }
-    data { "" }
+    status { ['Open', 'Closed'].sample }
+    data { {"passenger" =>
+              {"standard_lanes" =>
+                 {"lanes_open" => 2, "delay_minutes" => 5, "operational_status" => "no delay"}},
+            "commercial" => {"standard_lanes" => {"operational_status" => "Lanes Closed"}},
+            "pedestrian" =>
+              {"standard_lanes" =>
+                 {"lanes_open" => 2, "delay_minutes" => 1, "operational_status" => "no delay"}}} }
     number { "250501" }
   end
 end
