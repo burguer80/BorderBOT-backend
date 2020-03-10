d566dc5b8737fe9ab918e8dfeb5d2ac7c771092d
Sergio Burgueño <burguer@gmail.com>
2020-03-09 20:44:53 -0700

    add bwt job wip

--- app/services/btw_api_service.rb
+++ app/services/btw_api_service.rb
@@ -41,14 +41,13 @@ class BtwApiService
     end
   end
 
-  def get_port_btw(port_number, date)
-    encoded_date = date.strftime("%Y-%m-%d")
-    url = "https://bwt.cbp.gov/api/bwtwaittimegraph/09#{port_number}/#{encoded_date}"
+  def get_port_btw(port_number, date_param)
+    url = "https://bwt.cbp.gov/api/bwtwaittimegraph/09#{port_number}/#{date_param}"
     json_data = get_json(url)
     data = {commercial: json_data[0]["commercial_time_slots"]["commercial_slot"],
             private: json_data[0]["private_time_slots"]["private_slot"],
             pedestrain: json_data[0]["pedestrain_time_slots"]["pedestrain_slot"]}
-
+    date = Date.parse(date_param)
     pwt = PortWaitTime.where(port_number: 250302, date: date.all_day).first_or_create
     pwt.port_number = port_number
     pwt.date = Date.strptime(json_data[0]["date"], "%Y-%m-%d")
