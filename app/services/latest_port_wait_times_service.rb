64f07144f1b38e32f52ccdaa184cfb4b53914a3b
Sergio Burgueño <burguer@gmail.com>
2020-10-17 20:44:41 -0700

    ⚡️ code improvement

--- app/services/latest_port_wait_times_service.rb
+++ app/services/latest_port_wait_times_service.rb
@@ -1,20 +1,37 @@
 # frozen_string_literal: true
 
-class PortWaitTimesService
+class LatestPortWaitTimesService
+
+  def self.call
+    extract_ports
+  end
+
+  private
+
+  LATEST_PWT_JSON_URL = ENV['LATEST_PWT_JSON_URL']
 
   def self.extract_ports
     bwt_list = []
     get_latest_bwt_json.map do |bwt|
-      bwt_list << {
-        port_number: bwt['port_number'],
-        pulled_at: pulled_at_time_with_time_zone(bwt)
-      }
+      bwt_list << bwt_formatted(bwt)
     end
     bwt_list
   end
 
-  private
-  LATEST_PWT_JSON_URL = ENV['LATEST_PWT_JSON_URL']
+  def self.bwt_formatted(bwt)
+    port_detail = PortDetail.find_by(number: bwt['port_number'])
+    {
+      id: bwt['port_number'],
+      lanes: {
+        commercial: bwt['commercial_vehicle_lanes'],
+        pedestrian: bwt['pedestrian_lanes'],
+        private: bwt['passenger_vehicle_lanes']
+      },
+      details: port_detail.details,
+      hours: bwt['hours'],
+      pulled_at: "#{bwt['date']} #{bwt['time']} #{port_detail.time_zone}"
+    }
+  end
 
   def self.get_latest_bwt_json
     http = HttpService.new(LATEST_PWT_JSON_URL)
@@ -23,10 +40,6 @@ class PortWaitTimesService
 
   def self.port_time_zone(bwt)
     update_time_from_vehicle_lanes(bwt['commercial_vehicle_lanes']) || update_time_from_vehicle_lanes(bwt['passenger_vehicle_lanes']) || update_time_from_vehicle_lanes(bwt['pedestrian_lanes'])
-  end
-
-  def self.pulled_at_time_with_time_zone(bwt)
-    "#{bwt['date']} #{bwt['time']} #{PortDetail.find_by(number: bwt['port_number']).time_zone}"
   end
 
   def self.update_time_from_vehicle_lanes(vehicle_lanes)
