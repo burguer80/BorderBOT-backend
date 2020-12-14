655521e8b58ba8619f533e1ca48bf9bac9ab7da7
Sergio Burgueño <burguer@gmail.com>
2020-12-13 20:33:22 -0800

    remove outdated code

--- app/models/historical_wait_time.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class HistoricalWaitTime < ApplicationRecord
-  validates :port_number, presence: true
-  validates :month_number, presence: true
-  validates_uniqueness_of :port_number, scope: [:month_number, :time_slot, :bwt_day]
-end
--- app/models/holiday_wait_time.rb
+++
@@ -1,8 +0,0 @@
-class HolidayWaitTime < ApplicationRecord
-  validates :port_number, presence: true
-  validates :holiday_date, presence: true
-  validates_uniqueness_of :port_number, scope: [:port_number,
-                                                :bwt_date,
-                                                :time_slot,
-                                                :holiday_date]
-end
