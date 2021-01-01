9b68140107cd9f7fd78d354883ecc13d62e2e3e5
Sergio Burgueño <burguer@gmail.com>
2021-01-01 15:37:06 -0800

    added Cache::Base specs

--- app/services/cache/base.rb
+++ app/services/cache/base.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 class Cache::Base
+  attr_reader :key_name
+
   def initialize(key_name)
-    @key_name = key_name
+    return false unless key_name.present?
+    @key_name = key_name.to_s
   end
 end
---
+++ spec/services/cache/base_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe Cache::Base do
+
+  context 'with valid key_name' do
+    it 'should return proper key_name' do
+      expected_key_name = 'some_key_name'
+      service = described_class.new(expected_key_name)
+      expect(service.key_name).to eq('some_key_name')
+    end
+  end
+
+  context 'with invalid key_name' do
+    it 'should return false is nil is passed as arg' do
+      service = described_class.new(nil)
+      expect(service.key_name).to be_falsey
+    end
+
+    it 'should return false is empty string is passed as arg' do
+      service = described_class.new('')
+      expect(service.key_name).to be_falsey
+    end
+  end
+
+end
