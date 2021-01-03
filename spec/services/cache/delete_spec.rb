e736b915f29910314c5ff08878fd39940a080f5b
Sergio Burgueño <burguer@gmail.com>
2021-01-03 11:57:06 -0800

    Added Cache::Delete specs

--- spec/services/cache/delete_spec.rb
+++ spec/services/cache/delete_spec.rb
@@ -1,35 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Cache::Delete do
-  let(:cache) { Rails.cache }
-  let(:memory_store) { ActiveSupport::Cache.lookup_store(:memory_store) }
+  describe "#call" do
+    let(:cache) { Rails.cache }
+    let(:memory_store) { ActiveSupport::Cache.lookup_store(:memory_store) }
 
-  before do
-    allow(Rails).to receive(:cache).and_return(memory_store)
-    Rails.cache.clear
-  end
-
-  context 'with valid key name' do
-    subject(:service) { described_class.new(:some_key) }
-
-    it 'should return true if key name provided exists and is deleted properly' do
+    it 'should invoke Rails.cache.delete with proper arg' do
       expected_hash = { some_key: 'some_value' }
+      key_name = expected_hash.keys.first.to_s
+      allow(Rails).to receive(:cache).and_return(memory_store)
+      allow(cache).to receive(:delete).once.with(key_name).and_return(true)
       cache.write('some_key', expected_hash)
 
-      expect(service.call).to eq true
+      expect(described_class.new('some_key').call).to be_truthy
     end
 
-
-    it 'should return false if key name provided does not exists' do
-      expect(service.call).to eq false
-    end
-  end
-
-  context 'with invalid key name' do
-    subject(:service) { described_class.new('') }
-
-    it 'should return false if no key name is empty' do
-      expect(service.call).to eq false
-    end
   end
 end
